a9dd72b8539a97e1b456c9856933c40b1a440b16
Woohyun Jo <bluewing1004@gmail.com>
2025-02-21 01:14:54 +0900

    Initial Commit

---
+++ TamagotchiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TamagotchiProject
+//
+//  Created by 조우현 on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
